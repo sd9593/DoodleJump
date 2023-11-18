114aa658f7f74a75e3f74737154b68b90afe3b44
sd9593 <99853114+sd9593@users.noreply.github.com>
2023-11-18 11:06:13 -0600

    Timeline set up; just moves doodle right

--- src/GamePane.java
+++ src/GamePane.java
@@ -1,12 +1,35 @@
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 public class GamePane extends Pane {
 
-    private Doodle doodle;
+    // doodle attributes
+    private Rectangle doodle;
+    private double doodleX = 500;
+    private double doodleY = 650;
+    private static final int doodleWidth = 20;
+    private static final int doodleHeight = 30;
+
+    // private Doodle doodle;
+
+    Timeline animation;
 
     public GamePane() {
+        doodle = new Rectangle(doodleX, doodleY, doodleWidth, doodleHeight);
+        doodle.setFill(Color.GREEN);
         getChildren().add(doodle);
+        animation = new Timeline(new KeyFrame(new Duration(300), e -> moveDoodle()));
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.play();
+    }
+
+    public void moveDoodle() {
+        doodleX += 10;
+        doodle.setX(doodleX);
     }
 }
