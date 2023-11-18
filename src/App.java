64b68f8e545b46418bf68217663ee6f98f677a99
sd9593 <99853114+sd9593@users.noreply.github.com>
2023-11-18 10:40:20 -0600

    Added quit button

--- src/App.java
+++ src/App.java
@@ -1,14 +1,31 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage myStage) {
+        // borderPane will have score at top, game in center, quit button on bottom
+        BorderPane borderPane = new BorderPane();
+
+        Button quit = new Button("Quit");
+
         GamePane gamePane = new GamePane();
 
-        Scene scene = new Scene(gamePane, 1000, 700);
+        borderPane.setCenter(gamePane);
+        borderPane.setAlignment(quit, Pos.CENTER);
+        borderPane.setBottom(quit);
+
+        quit.setOnAction((ActionEvent e) -> {
+            System.exit(0);
+        });
+
+        Scene scene = new Scene(borderPane, 1000, 700);
         myStage.setScene(scene);
         myStage.setTitle("Doodle jump");
         myStage.show();
