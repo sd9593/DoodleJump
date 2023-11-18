319663b3d6003e160939d13cf9d278235279bcf0
sd9593 <99853114+sd9593@users.noreply.github.com>
2023-11-18 11:36:18 -0600

    Implemented doodle falling animation

--- src/GamePane.java
+++ src/GamePane.java
@@ -14,17 +14,23 @@ public class GamePane extends Pane {
     private Rectangle doodle;
     private double doodleX = 500;
     private double doodleY = 600;
-    private static final int doodleWidth = 20;
-    private static final int doodleHeight = 30;
+    private static final int DOODLE_WIDTH = 20;
+    private static final int DOODLE_HEIGHT = 30;
     private double speed = 20;
+
+    // attributes for falling animation
+    private static final int GRAVITY = 100;
+    private static final double DURATION = 0.015;
+    private double velocity = 0;
 
     Timeline doodleAnimation;
 
     public GamePane() {
-        doodle = new Rectangle(doodleX, doodleY, doodleWidth, doodleHeight);
+        doodle = new Rectangle(doodleX, doodleY, DOODLE_WIDTH, DOODLE_HEIGHT);
         doodle.setFill(Color.GREEN);
         getChildren().add(doodle);
-        doodleAnimation = new Timeline(new KeyFrame(new Duration(300), e -> jump()));
+        doodleAnimation = new Timeline(new KeyFrame(new Duration(DURATION * GRAVITY * 10), e -> fall()));
+        // DURTION * GRAVITY * 10 allows better speed for falling
         doodleAnimation.setCycleCount(Timeline.INDEFINITE);
     }
 
@@ -33,20 +39,28 @@ public class GamePane extends Pane {
         doodleAnimation.play();
     }
 
+    public void fall() {
+        velocity = velocity + GRAVITY * DURATION;
+        doodleY = doodleY + velocity * DURATION;
+        doodle.setY(doodleY);
+    }
+
     public void jump() {
         doodleY += 10;
         doodle.setY(doodleY);
     }
 
     public void moveDoodle(KeyEvent e) {
+        // wrap around to right if doodle goes off screen
         if (e.getCode() == KeyCode.LEFT) {
             if (doodleX <= 0) {
                 doodleX = getWidth();
             }
             doodleX -= speed;
         } else if (e.getCode() == KeyCode.RIGHT) {
-            if (doodleX + doodleWidth >= getWidth()) {
-                doodleX = 0 - doodleWidth;
+            // wrap around to left if doodle goes off screen
+            if (doodleX + DOODLE_WIDTH >= getWidth()) {
+                doodleX = 0 - DOODLE_WIDTH;
             }
             doodleX += speed;
         }
