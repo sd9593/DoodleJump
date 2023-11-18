361fbcf69e568f2d3547fbf0218c5738f6dfc635
sd9593 <99853114+sd9593@users.noreply.github.com>
2023-11-18 10:29:04 -0600

    Blank stage appears

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage myStage) {
+        GamePane gamePane = new GamePane();
+
+        Scene scene = new Scene(gamePane, 1000, 700);
+        myStage.setScene(scene);
+        myStage.setTitle("Doodle jump");
+        myStage.show();
+        myStage.setResizable(false);
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Application.launch();
     }
 }
